cc857831100ceb1cc269237fb24fc20c5ca31363
zignna <tlswlsdk14@naver.com>
2025-12-30 21:38:10 +0900

    fix license typo

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,6 +1,6 @@
 // Token : smart contract based
 // BIT, ETH, XRP, KAIA : native token
-// SPDX-License-Identifer:MIT
+// SPDX-License-Identifier:MIT
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.28;
 
