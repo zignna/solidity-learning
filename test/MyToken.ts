697fa010328359669354fd21329831e17fa9b1b2
zignna <tlswlsdk14@naver.com>
2025-12-30 22:11:15 +0900

    add missing

--- test/MyToken.ts
+++ test/MyToken.ts
@@ -1,11 +1,13 @@
 import hre from "hardhat";
 import { expect } from "chai";
 import { MyToken } from "../typechain-types";
+import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
 
 describe("mytoken deploy", () => {
   let myTokenC: MyToken;
   let signers: HardhatEthersSigner[];
   before("should deploy", async () => {
+    signers = await hre.ethers.getSigners();
     myTokenC = await hre.ethers.deployContract("MyToken", [
       "MyToken",
       "MT",
@@ -22,10 +24,19 @@ describe("mytoken deploy", () => {
     expect(await myTokenC.decimals()).equal(18);
   });
   it("should return 0 totalSupply", async () => {
-    expect(await myTokenC.totalSupply()).equal(0);
+    expect(await myTokenC.totalSupply()).equal(1n * 10n ** 18n);
   });
-  it("should return 0 balance for signer 0", async () => {
-    const signers = await hre.ethers.getSigners();
-    expect(await myTokenC.balanceOf(signers[0].address)).equal(0);
+  // 1MT = 1*10^18
+
+  it("should return 1MT balance for signer 0", async () => {
+    const signer0 = signers[0];
+    expect(await myTokenC.balanceOf(signer0)).equal(1n * 10n ** 18n);
+  });
+  it("should have 0.5MT", async () => {
+    const signer1 = signers[1];
+    await myTokenC.transfer(hre.ethers.parseUnits("1.1", 18), signer1.address);
+    // expect(await myTokenC.balanceOf(signer1)).equal(
+    //   hre.ethers.parseUnits("0.5", 18)
+    // );
   });
 });
