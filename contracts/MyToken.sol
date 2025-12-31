74ba4a9679c23ed4ae8bfdce33e6b2a3143881d7
zignna <tlswlsdk14@naver.com>
2025-12-31 15:33:48 +0900

    fix event typo

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.28;
 
 contract MyToken {
     event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed speder, uint256 amount);
+    event Approval(address indexed spender, uint256 amount);
     string public name;
     string public symbol;
     uint8 public decimals; //1 ETH --> 1*10^18 wei / 1 wei --> 1*10^-18
