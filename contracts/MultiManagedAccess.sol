3a01d30be152870fe5199e2918db6abd8c8d5a5b
zignna <tlswlsdk14@naver.com>
2026-01-06 21:02:10 +0900

    add multi managed access

---
+++ contracts/MultiManagedAccess.sol
@@ -0,0 +1,62 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+abstract contract MultiManagedAccess {
+    uint constant MANAGER_NUMBERS = 5;
+    uint immutable BACKUP_MANGER_NUMBERS;
+
+    address public owner;
+    address[MANAGER_NUMBERS] public managers;
+    bool[MANAGER_NUMBERS] public confirmed;
+    mapping(uint256 => bool[MANAGER_NUMBERS]) public confirmedRq;
+
+    // manager0 --> confirmed0
+    // manager1 --> confirmed1
+
+    constructor(address _owner, address[] memory _managers, uint _manager_numbers) {
+        require(_managers.length == _manager_numbers, "size unmatched");
+        owner = _owner;
+        BACKUP_MANGER_NUMBERS = _manager_numbers;
+        for(uint i=0; i<MANAGER_NUMBERS; i++) {
+            managers[i] = _managers[i];
+        }
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You are not authorized");
+        _;
+    }
+
+    function allConfirmed()  internal view returns (bool) {
+        for(uint i=0; i<MANAGER_NUMBERS; i++) {
+            if(!confirmed[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function reset()   internal {
+        for(uint i=0; i<MANAGER_NUMBERS;i++) {
+            confirmed[i] = false;
+        }
+    }
+
+    modifier onlyAllConfirmed() {
+        require(allConfirmed(), "Not all managers confirmed yet");
+        reset();
+        _;
+    }
+
+    function confirm()  external  {
+        bool found = false;
+        for(uint i=0; i<MANAGER_NUMBERS; i++) {
+            if(managers[i] == msg.sender) {
+                found = true;
+                confirmed[i] = true;
+                break;
+            }
+        }
+        require(found, "You are not one of managers");
+    }
+}
